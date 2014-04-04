f0407be528023b7e6c53e13c396887c464748106
Mike Dunn <mike@eikonomega.com>
2014-04-04 14:02:11 -0400

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Flask-Authorization-Panda
+-------------
+
+Flask Authorization for Pandas!
+
+Provides decorators for Flask view methods that implement various
+authentication schemes.
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Authorization-Panda',
+    version='0.0',
+    url='https://github.com/eikonomega/flask-authorization-panda',
+    license='MIT',
+    author='Mike Dunn',
+    author_email='mike@eikonomega.com',
+    description='Flask Authorization for Pandas!',
+    long_description=__doc__,
+    packages=['flask_authorization-panda'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'PyTest'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
