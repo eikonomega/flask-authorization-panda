52a256e85a7f4d7ea101c84c5804da108debf5bf
Mike Dunn <mike@eikonomega.com>
2014-04-07 13:59:35 -0400

    Strip a couple of parameters form setup.py that may be causing problems during pypi deployment.

--- flask_authorization_panda/__init__.py
+++ flask_authorization_panda/__init__.py
@@ -1,7 +1,53 @@
 """
-Provides decorators that can be applied to Flask view methods to perform
-various authentication checks.
+**Flask-Authorization-Panda provides easy loading and access to the data elements of
+JSON based configuration files.**
+
+Usage
+-----
+
+Assuming that an environment variable 'SHARED_CONFIG_FILES' exists
+and points to a directory containing multiple JSON files, including
+the following::
+
+    ldap.json
+      {
+        "primary": {
+            "url": "ldaps://primaryldap.example.edu:111",
+            "login": "cn=LDAP Testing",
+            "password": "LDAP Password"
+        }
+      }
+
+    smtp.json
+      {
+        "TestAccount1": {
+            "url": "smtp.yourschool.edu",
+            "login": "testaccount1",
+            "password": "testaccount1password"
+        }
+      }
+
+You would access the contents of those configuration files like this::
+
+    >>> from configuration_panda import ConfigurationPanda
+    >>> program_settings = ConfigurationPanda(['SHARED_CONFIG_FILES'])
+    >>> program_settings.ldap['primary']['url']
+    ldaps://primaryldap.example.edu:111
+    >>> program_settings.smtp['TestAccount1']['login']
+    testaccount1
+
+Or, if you prefer dictionary-style syntax::
+
+    >>> from configuration_panda import ConfigurationPanda
+    >>> program_settings = ConfigurationPanda(['SHARED_CONFIG_FILES'])
+    >>> program_settings['ldap']['primary']['url']
+    ldaps://primaryldap.example.edu:111
+    >>> program_settings['smtp']['TestAccount1']['login']
+    testaccount1
+
 
 """
+
+__version__ = '0.5'
 
 from basic_auth import basic_auth
--- setup.py
+++ setup.py
@@ -22,9 +22,7 @@ setup(
     description='Flask Authorization for Pandas!',
     long_description=flask_authorization_panda.__doc__,
     packages=find_packages(),
-    zip_safe=False,
     include_package_data=True,
-    platforms='any',
     install_requires=[
         'Flask',
         'PyTest'
@@ -38,3 +36,30 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
+
+
+# from setuptools import setup, find_packages
+#
+# import configuration_panda
+#
+# with open('requirements.txt') as requirements_doc:
+#     requirements = requirements_doc.read()
+#
+# setup(
+#     name="configuration-panda",
+#     packages=find_packages(),
+#     version=configuration_panda.__version__,
+#     author="Mike Dunn",
+#     author_email="mike@eikonomega.com",
+#     url="https://github.com/eikonomega/configuration_panda",
+#     description="ConfigurationPanda provides easy loading and access to "
+#                 "the data elements of JSON based configuration files.",
+#     long_description=configuration_panda.__doc__,
+#
+#     install_requires=requirements,
+#     #include_package_data=True,
+#     package_data={
+#         '': ['requirements.txt']
+#     }
+# )
+
